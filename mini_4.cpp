89eaf88121477bd646fa8bcaf7fbb3985ddcf61b
Wokzy <yegor6741@gmail.com>
2026-04-11 16:45:18 +0700

    mini 3, 4 review 2
    Signed-off-by: Wokzy <yegor6741@gmail.com>

--- mini_4.cpp
+++ mini_4.cpp
@@ -14,168 +14,217 @@ void my_swap(T& a, T& b) {
 	b = move(tmp);
 }
 
-struct Treap {
-	int priority;
-	int sum;
-	int size;
-	int value;
-	shared_ptr<Treap> right;
-	shared_ptr<Treap> left;
+class Treap {
+public:
+	struct TreapNode {
+		int priority;
+		int sum;
+		int size;
+		int value;
+		shared_ptr<TreapNode> right;
+		shared_ptr<TreapNode> left;
 
-	Treap(int p, int val) : sum(val), value(val), priority(p), size(1), right(nullptr), left(nullptr) {}
-	Treap(const Treap &other) : priority(other.priority), sum(other.sum), size(other.size),
-								value(other.value) {
+		TreapNode(int p, int val) : sum(val), value(val), priority(p), size(1), right(nullptr), left(nullptr) {}
+		TreapNode(const TreapNode &other) : priority(other.priority), sum(other.sum), size(other.size),
+									value(other.value) {
 
-		if (other.right != nullptr) {
-			right = make_shared<Treap>(*other.right);
+			if (other.right != nullptr) {
+				right = make_shared<TreapNode>(*other.right);
+			}
+
+			if (other.left != nullptr) {
+				left = make_shared<TreapNode>(*other.left);
+			}
 		}
 
-		if (other.left != nullptr) {
-			left = make_shared<Treap>(*other.left);
+		TreapNode(TreapNode &&other) : priority(other.priority), sum(other.sum), size(other.size),
+							value(other.value) {
+
+			my_swap(right, other.right);
+			my_swap(left, other.left);
+		}
+
+		TreapNode &operator=(const TreapNode &other) {
+			priority = other.priority;
+			sum = other.sum;
+			size = other.size;
+			value = other.value;
+
+			if (other.right != nullptr) {
+				right = make_shared<TreapNode>(*other.right);
+			}
+
+			if (other.left != nullptr) {
+				left = make_shared<TreapNode>(*other.left);
+			}
+
+			return *this;
+		}
+
+		TreapNode &operator=(TreapNode &&other) {
+			priority = other.priority;
+			sum = other.sum;
+			size = other.size;
+			value = other.value;
+			my_swap(right, other.right);
+			my_swap(left, other.left);
+
+			return *this;
+		}
+
+		void update() {
+			size = 1;
+			sum = value;
+			if (left) {
+				size += left->size;
+				sum += left->sum;
+			}
+			if (right) {
+				size += right->size;
+				sum += right->sum;
+			}
+		}
+	};
+
+protected:
+
+	shared_ptr<TreapNode> merge(shared_ptr<TreapNode> x, shared_ptr<TreapNode> y) {
+		if (x == nullptr) return y;
+		if (y == nullptr) return x;
+
+		if (x->priority < y->priority) {
+			x->right = merge(x->right, y);
+			x->right->update();
+			x->update();
+			return x;
+		}
+
+		y->left = merge(x, y->left);
+		y->left->update();
+		y->update();
+		return y;
+	}
+
+
+	pair<shared_ptr<TreapNode>, shared_ptr<TreapNode>> splitBySize(shared_ptr<TreapNode> x, int k) {
+		if (x == nullptr) return {nullptr, nullptr};
+
+		int l_size = 0;
+		if (x->left) l_size = x->left->size;
+
+		if (k <= l_size) {
+			auto ll_lr = splitBySize(x->left, k);
+			x->left = ll_lr.second;
+			x->update();
+			return {ll_lr.first, x};
+		}
+
+		auto rl_rr = splitBySize(x->right, k - l_size - 1);
+		x->right = rl_rr.first;
+		x->update();
+		return {x, rl_rr.second};
+	}
+
+
+	shared_ptr<TreapNode> insert(shared_ptr<TreapNode> x, int pos, int value) {
+		auto l_r = splitBySize(x, pos - 1);
+		auto y = make_shared<TreapNode>(rand(), value);
+		return merge(merge(l_r.first, y), l_r.second);
+	}
+
+	shared_ptr<TreapNode> erase(shared_ptr<TreapNode> x, int pos, int count) {
+		auto l_r = splitBySize(x, pos - 1);
+		auto rl_rr = splitBySize(l_r.second, count);
+
+		return merge(l_r.first, rl_rr.second);
+	}
+
+
+	shared_ptr<TreapNode> hp_node; // highest-priority node
+
+public:
+
+	Treap(): hp_node(nullptr) {}
+	Treap(shared_ptr<TreapNode> node): hp_node(node) {}
+
+	Treap(Treap &&other) {
+		my_swap(hp_node, other.hp_node);
+	}
+
+	Treap(const Treap &other): hp_node(nullptr) {
+		if (other.hp_node != nullptr) {
+			hp_node = make_shared<TreapNode>(*other.hp_node);
 		}
 	}
 
-	Treap(Treap &&other) : priority(other.priority), sum(other.sum), size(other.size),
-						value(other.value) {
-
-		my_swap(right, other.right);
-		my_swap(left, other.left);
+	Treap &operator=(shared_ptr<TreapNode> node) {
+		hp_node = node;
+		return *this;
 	}
 
 	Treap &operator=(const Treap &other) {
-		priority = other.priority;
-		sum = other.sum;
-		size = other.size;
-		value = other.value;
-
-		if (other.right != nullptr) {
-			right = make_shared<Treap>(*other.right);
-		}
-
-		if (other.left != nullptr) {
-			left = make_shared<Treap>(*other.left);
+		if (other.hp_node != nullptr) {
+			hp_node = make_shared<TreapNode>(*other.hp_node);
 		}
 
 		return *this;
 	}
 
 	Treap &operator=(Treap &&other) {
-		priority = other.priority;
-		sum = other.sum;
-		size = other.size;
-		value = other.value;
-		my_swap(right, other.right);
-		my_swap(left, other.left);
+		my_swap(hp_node, other.hp_node);
 
 		return *this;
 	}
 
-	void update() {
-		size = 1;
-		sum = value;
-		if (left) {
-			size += left->size;
-			sum += left->sum;
+	int sum(int lq, int rq) {
+		auto l_r = splitBySize(hp_node, lq - 1);
+		auto rl_rr = splitBySize(l_r.second, rq - lq + 1);
+
+		int res = 0;
+		if (rl_rr.first != nullptr) {
+			res = rl_rr.first->sum;
 		}
-		if (right) {
-			size += right->size;
-			sum += right->sum;
-		}
+
+		merge(l_r.first, merge(rl_rr.first, rl_rr.second));
+		return res;
+	}
+
+	void insert(int pos, int value) {
+		hp_node = insert(hp_node, pos, value);
+	}
+
+	void erase(int pos, int count) {
+		hp_node = erase(hp_node, pos, count);
 	}
 };
 
 
-shared_ptr<Treap> merge(shared_ptr<Treap> x, shared_ptr<Treap> y) {
-	if (x == nullptr) return y;
-	if (y == nullptr) return x;
-
-	if (x->priority < y->priority) {
-		x->right = merge(x->right, y);
-		x->right->update();
-		x->update();
-		return x;
-	}
-
-	y->left = merge(x, y->left);
-	y->left->update();
-	y->update();
-	return y;
-}
+void test_1(Treap &treap) {
+	assert(treap.sum(1, 1) == 1);
+	assert(treap.sum(1, 10) == 75);
+	assert(treap.sum(3, 4) == 35);
+	assert(treap.sum(100, 230) == 0);
+	assert(treap.sum(4, 4) == 25);
 
 
-pair<shared_ptr<Treap>, shared_ptr<Treap>> splitBySize(shared_ptr<Treap> x, int k) {
-	if (x == nullptr) return {nullptr, nullptr};
-
-	int l_size = 0;
-	if (x->left) l_size = x->left->size;
-
-	if (k <= l_size) {
-		auto ll_lr = splitBySize(x->left, k);
-		x->left = ll_lr.second;
-		x->update();
-		return {ll_lr.first, x};
-	}
-
-	auto rl_rr = splitBySize(x->right, k - l_size - 1);
-	x->right = rl_rr.first;
-	x->update();
-	return {x, rl_rr.second};
-}
-
-
-shared_ptr<Treap> insert(shared_ptr<Treap> x, int pos, int value) {
-	auto l_r = splitBySize(x, pos - 1);
-	auto y = make_shared<Treap>(rand(), value);
-	return merge(merge(l_r.first, y), l_r.second);
-}
-
-shared_ptr<Treap> erase(shared_ptr<Treap> x, int pos, int count) {
-	auto l_r = splitBySize(x, pos - 1);
-	auto rl_rr = splitBySize(l_r.second, count);
-
-	return merge(l_r.first, rl_rr.second);
-}
-
-int sum(shared_ptr<Treap> x, int lq, int rq) {
-	auto l_r = splitBySize(x, lq - 1);
-	auto rl_rr = splitBySize(l_r.second, rq - lq + 1);
-
-	int res = 0;
-	if (rl_rr.first != nullptr) {
-		res = rl_rr.first->sum;
-	}
-
-	merge(l_r.first, merge(rl_rr.first, rl_rr.second));
-	return res;
-}
-
-
-void test_1(shared_ptr<Treap> treap) {
-	assert(sum(treap, 1, 1) == 1);
-	assert(sum(treap, 1, 10) == 75);
-	assert(sum(treap, 3, 4) == 35);
-	assert(sum(treap, 100, 230) == 0);
-	assert(sum(treap, 4, 4) == 25);
-
-
-	treap = erase(treap, 3, 2);
+	treap.erase(3, 2);
 	// {1, 14, 25};
-	assert(sum(treap, 1, 10) == 40);
-	assert(sum(treap, 2, 2) == 14);
+	assert(treap.sum(1, 10) == 40);
+	assert(treap.sum(2, 2) == 14);
 }
 
-void test_2(shared_ptr<Treap> treap) {
-	assert(sum(treap, 1, 1) == 1);
-	assert(sum(treap, 1, 10) == 55);
-	assert(sum(treap, 3, 4) == 15);
-	assert(sum(treap, 100, 230) == 0);
-	assert(sum(treap, 4, 4) == 5);
+void test_2(Treap &treap) {
+	assert(treap.sum(1, 1) == 1);
+	assert(treap.sum(1, 10) == 55);
+	assert(treap.sum(3, 4) == 15);
+	assert(treap.sum(100, 230) == 0);
+	assert(treap.sum(4, 4) == 5);
 
 
-	treap = erase(treap, 3, 2);
+	treap.erase(3, 2);
 	// {1, 14, 25};
-	assert(sum(treap, 1, 10) == 40);
-	assert(sum(treap, 2, 2) == 14);
+	assert(treap.sum(1, 10) == 40);
+	assert(treap.sum(2, 2) == 14);
 }
 
 
@@ -185,19 +234,21 @@ int main(void) {
 
 	srand((unsigned)time(0));
 
-	auto test = insert(nullptr, 1, 1);
-	test = insert(test, 2, 14);
-	test = insert(test, 3, 25);
-	test = insert(test, 4, 25);
-	test = insert(test, 3, 10);
+	Treap test{};
+	test.insert(1, 1);
+	test.insert(2, 14);
+	test.insert(3, 25);
+	test.insert(4, 25);
+	test.insert(3, 10);
 
-	auto test_moved = insert(nullptr, 1, 1);
-	test_moved = insert(test_moved, 2, 14);
-	test_moved = insert(test_moved, 3, 5);
-	test_moved = insert(test_moved, 4, 25);
-	test_moved = insert(test_moved, 3, 10);
+	Treap test_moved{};
+	test_moved.insert(1, 1);
+	test_moved.insert(2, 14);
+	test_moved.insert(3, 5);
+	test_moved.insert(4, 25);
+	test_moved.insert(3, 10);
 
-	my_swap(*test, *test_moved);
+	my_swap(test, test_moved);
 
 
 	// {1, 14, 10, 25, 25}
